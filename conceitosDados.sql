68fa53bf421e651be151599dc183c8cc7b5f9c29
Lucas Celis <celis.lucase@gmail.com>
2026-04-01 11:49:36 -0300

    Create conceitosDados.sql

---
+++ conceitosDados.sql
@@ -0,0 +1,100 @@
+/* DML - DATA MANIPULATION LANGUAGE */
+/* INSERT - Irá inserir dados na tabela */
+
+INSERT INTO CLIENTE VALUES(NULL, 'PAULA', 'M', NULL, '77437493');
+
+SELECT * FROM CLIENTE
+WHERE SEXO = 'M';
+
++-----------+--------+------+----------------+-------------+
+| IDCLIENTE | NOME   | SEXO | EMAIL          | CPF         |
++-----------+--------+------+----------------+-------------+
+|         1 | JOAO   | M    | JOAOA@IG.COM   | 76567587887 |
+|         2 | CARLOS | M    | CARLOSA@IG.COM | 5464553466  |
+|         5 | JORGE  | M    | JORGE@IG.COM   | 8756547688  |
+|         6 | CELIA  | M    | JCELIA@IG.COM  | 5767876889  |
+|         7 | PAULA  | M    | NULL           | 77437493    |
++-----------+--------+------+----------------+-------------+
+
+/* UPDATE - Irá atualizar determinado dado da tabela */
+
+SELECT * FROM CLIENTE
+WHERE IDCLIENTE = 7;
+
++-----------+--------+------+----------------+-------------+
+| IDCLIENTE | NOME   | SEXO | EMAIL          | CPF         |
++-----------+--------+------+----------------+-------------+
+|         7 | PAULA  | M    | NULL           | 77437493    |
++-----------+--------+------+----------------+-------------+
+
+UPDATE CLIENTE
+	SET SEXO = 'F'
+WHERE IDCLIENTE = 7;
+
+SELECT * FROM CLIENTE
+WHERE IDCLIENTE = 7;
+
++-----------+--------+------+----------------+-------------+
+| IDCLIENTE | NOME   | SEXO | EMAIL          | CPF         |
++-----------+--------+------+----------------+-------------+
+|         7 | PAULA  | F    | NULL           | 77437493    |
++-----------+--------+------+----------------+-------------+
+
+INSERT INTO CLIENTE VALUES(NULL, 'XXX', 'M', NULL, 'XXXX');
+
+/* DELETE - Irá deletar determinado dado da tabela */
+
+SELECT * FROM CLIENTE
+WHERE IDCLIENTE = 8;
+
++-----------+--------+------+----------------+-------------+
+| IDCLIENTE | NOME   | SEXO | EMAIL          | CPF         |
++-----------+--------+------+----------------+-------------+
+|         8 | XXX    | M    | NULL           | XXXX        |
++-----------+--------+------+----------------+-------------+
+
+DELETE FROM CLIENTE
+WHERE IDCLIENTE = 8;
+
+/* DDL - DATA DEFINITION LANGUAGE */
+
+CREATE TABLE PRODUTO(
+	IDPRODUTO INT PRIMARY KEY AUTO_INCREMENT,
+	NOME_PRODUTO VARCHAR(30) NOT NULL,
+	PRECO INT,
+	FRETE FLOAT(10,2) NOT NULL
+);
+
+/* ALTER TABLE - Irá alterar uma coluna da tabela */
+
+/* CHANGE - Altera as definições e nome da coluna */
+
+ALTER TABLE PRODUTO 
+CHANGE PRECO VALOR_UNITARIO INT NOT NULL;
+
+/* MODIFY - Altera as definições da coluna */
+
+ALTER TABLE PRODUTO
+MODIFY VALOR_UNITARIO VARCHAR(50) NOT NULL;
+
+/* ADD - Adicionar uma coluna */
+
+ALTER TABLE PRODUTO
+ADD PESO FLOAT(10,2);
+
+/* DROP COLUMN - Apagar uma coluna */
+
+ALTER TABLE PRODUTO
+DROP COLUMN PESO;
+
+/* AFTER - Inserir a coluna após outra específica da tabela */
+
+ALTER TABLE PRODUTO
+ADD COLUMN PESO FLOAT(10,2) NOT NULL
+AFTER NOME_PRODUTO;
+
+/* FIRST - Inserir a coluna na primeira posição da tabela */
+
+ALTER TABLE PRODUTO
+ADD COLUMN PESO FLOAT(10,2) NOT NULL
+FIRST;
